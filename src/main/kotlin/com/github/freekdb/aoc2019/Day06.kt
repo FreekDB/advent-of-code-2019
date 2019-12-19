ac156a7d90c9397d363722d89c1b00bb82d8d613
Freek de Bruijn <freekdb@gmail.com>
2019-12-19 23:19:33 +0100

    Day 6: Universal Orbit Map

--- src/main/kotlin/com/github/freekdb/aoc2019/Day06.kt
+++ src/main/kotlin/com/github/freekdb/aoc2019/Day06.kt
@@ -3,10 +3,13 @@ package com.github.freekdb.aoc2019
 import java.io.File
 
 private const val CENTER_OF_MASS = "COM"
+private const val SANTA = "SAN"
+private const val YOU = "YOU"
 
 // https://adventofcode.com/2019/day/6
 fun main() {
     val orbitMap = mutableMapOf<String, MutableList<String>>()
+    val reverseOrbitMap = mutableMapOf<String, String>()
 
     File("input/day-06--input.txt")
         .readLines()
@@ -18,12 +21,26 @@ fun main() {
 
             val destinations = orbitMap.getOrPut(sourceObject, { mutableListOf() })
             destinations.add(destinationObject)
+
+            reverseOrbitMap[destinationObject] = sourceObject
         }
 
     val orbitCountMap = mutableMapOf(CENTER_OF_MASS to 0)
-    extendOrbitCountMap(CENTER_OF_MASS, orbitMap, orbitCountMap)
+    extendOrbitCountMap(
+        CENTER_OF_MASS,
+        orbitMap,
+        orbitCountMap
+    )
 
+    // Part 1.
     println("Total number of direct and indirect orbits: ${orbitCountMap.values.sum()}.")
+
+    // Part 2.
+    val pathToYou = getPathToStart(YOU, reverseOrbitMap)
+    val pathToSanta = getPathToStart(SANTA, reverseOrbitMap)
+    val firstIntersection = pathToYou.intersect(pathToSanta).first()
+    val minimumOrbitalTransfers = pathToYou.indexOf(firstIntersection) - 1 + pathToSanta.indexOf(firstIntersection) - 1
+    println("Minimum number of orbital transfers: $minimumOrbitalTransfers")
 }
 
 fun extendOrbitCountMap(sourceObject: String, orbitMap: Map<String, List<String>>,
@@ -37,3 +54,8 @@ fun extendOrbitCountMap(sourceObject: String, orbitMap: Map<String, List<String>
         extendOrbitCountMap(it, orbitMap, orbitCountMap)
     }
 }
+
+fun getPathToStart(finalDestination: String, reverseOrbitMap: MutableMap<String, String>) =
+    generateSequence(finalDestination) { objectName ->
+        reverseOrbitMap[objectName].takeIf { it != CENTER_OF_MASS }
+    }.toList()
